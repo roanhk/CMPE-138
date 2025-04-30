48fb53f5e074612837c4fb3d50cf442589cc61df
rohankhadka <rohan.khadka@gmail.com>
2025-04-29 23:54:52 -0700

    I put my queries

---
+++ Query1.sql
@@ -0,0 +1,25 @@
+
+SELECT
+  MAX(country_name) AS country_name,
+  country_code,
+  MAX(population) AS population,
+  MAX(cumulative_deceased) AS total_deaths,
+  MAX(cumulative_deceased_age_5) AS total_deaths_age_5,
+  MAX(cumulative_deceased_age_6) AS total_deaths_age_6,
+  MAX(cumulative_deceased_age_9) AS total_deaths_age_9,
+  MAX(
+    IFNULL(cumulative_deceased_age_5, 0) +
+    IFNULL(cumulative_deceased_age_6, 0) +
+    IFNULL(cumulative_deceased_age_9, 0)
+  ) AS total_deaths_aged_5_6_9,
+  MAX(date) AS latest_data_date
+FROM
+  `bigquery-public-data.covid19_open_data.covid19_open_data`
+WHERE
+  country_code IS NOT NULL
+  AND population > 50000000
+GROUP BY
+  country_code
+ORDER BY
+  total_deaths DESC
+LIMIT 15;
---
+++ Query2.sql
@@ -0,0 +1,21 @@
+SELECT
+  country_name,
+  country_code,
+  population,
+  health_expenditure_usd,
+  ROUND(health_expenditure_usd * population, 0) AS total_health_spending_usd,
+  nurses_per_1000,
+  physicians_per_1000,
+  out_of_pocket_health_expenditure_usd
+FROM
+  `bigquery-public-data.covid19_open_data.covid19_open_data`
+WHERE
+  country_code IS NOT NULL
+  AND population > 50000000
+  AND health_expenditure_usd IS NOT NULL
+  AND date = (SELECT MAX(date) FROM `bigquery-public-data.covid19_open_data.covid19_open_data`)
+ORDER BY
+  total_health_spending_usd DESC
+  LIMIT 15;
+
+
